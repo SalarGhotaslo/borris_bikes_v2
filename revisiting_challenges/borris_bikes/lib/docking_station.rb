65c12f1cef411f3f21f62bcd6f528fa64d94ca5a
Salar Ghotaslo <Salar_@live.co.uk>
2021-02-07 15:23:00 +0000

    docks a bike method and shows a bike has been docked added

--- revisiting_challenges/borris_bikes/lib/docking_station.rb
+++ revisiting_challenges/borris_bikes/lib/docking_station.rb
@@ -1,7 +1,17 @@
 require_relative 'bike'
 
+# def initialize(bike)
+#   @bike = bike
+# end
+
 class DockingStation
+  attr_reader :bike
+
   def release_bike
     Bike.new
+  end
+
+  def dock_bike(bike)
+    @bike = bike
   end
 end
--- revisiting_challenges/borris_bikes/spec/docking_station_spec.rb
+++ revisiting_challenges/borris_bikes/spec/docking_station_spec.rb
@@ -8,4 +8,18 @@ describe DockingStation do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+  it 'docks a bike' do
+    expect(subject).to respond_to(:dock_bike).with(1).argument
+  end
+
+  it 'shows a bike has been docked' do
+    bike = Bike.new
+    subject.dock_bike(bike)
+    expect(subject.dock_bike(bike)).to eq bike
+  end
+
+  it 'responds to bike' do
+    expect(subject).to respond_to(:bike)
+  end
 end
